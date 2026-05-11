0e7470a4f6e21ed6c6dd519c630c043ca1130477
Ragavan-knowsdtech <24058rr@hvhs.school.nz>
2026-05-12 10:59:54 +1200

    Nah

--- t08_scope.js
+++ t08_scope.js
@@ -1,24 +1,18 @@
 const output = document.getElementById("output");
+let myVar = 0; // Use let for variables that change
 
-/* Read the slides, make a prediction, then uncomment this code.
-
-
-var myVar = 0;
-output.innerHTML = "Start: "+myVar+"<br>";
-
-functionOne();
 function functionOne() {
   myVar = 1;
-  output.innerHTML += "functionOne: "+myVar+"<br>";
+  output.innerHTML += `functionOne: ${myVar}<br>`;
 }
 
-functionTwo();
 function functionTwo() {
-  var myVar = 2;
-  output.innerHTML += "functionTwo: "+myVar+"<br>";
+  myVar = 2; 
+  output.innerHTML += `functionTwo: ${myVar}<br>`;
 }
 
-output.innerHTML += "end: "+myVar+"<br>";
-
-
-*/
+// Call functions 
+output.innerHTML = `Start: ${myVar}<br>`;
+functionOne();
+functionTwo();
+output.innerHTML += `end: ${myVar}<br>`; 
