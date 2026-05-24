d116cc1fc894e86e1e77ba5ca5b815598e1be205
Ragavan-knowsdtech <24058rr@hvhs.school.nz>
2026-05-25 09:55:28 +1200

    BAnana

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,17 +1,20 @@
 console.log("t10_input_from_HTML.js");
 console.log("Hello World");
 
-// Variables
+/****************************
+Variables
+****************************/
 let name = "Ragavan";
 let age = 15;
 let year = 2026;
 let money = 10;
 
-/****************************
-Main code:
-****************************/
+const NAME_FIELD = document.getElementById("nameField");
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
+/****************************
+Main code
+****************************/
 OUTPUT.innerHTML = "<h3>Hello World</h3>";
 OUTPUT.innerHTML += "<h3>Cheese Burger</h3>";
 OUTPUT.innerHTML += "<h3>NOT GOOD</h3>";
@@ -19,16 +22,14 @@ OUTPUT.innerHTML += "<h3>BADABABACHING I'm Not Lov'in It</h3>";
 
 OUTPUT.innerHTML +=
   "<h3>Hello " + name +
-  " You are " + age +
-  " The Year is " + year +
-  " You have $" + money +
-  " You spend half of your money, now you have $" + (money / 2) +
-  " Then you get $3, now you have $" + ((money / 2) + 3) +
-  "</h3>";
+  ". You are " + age +
+  ". The year is " + year +
+  ". You have $" + money +
+  ". You spend half your money, now you have $" + (money / 2) +
+  ". Then you get $3, now you have $" + ((money / 2) + 3) +
+  ".</h3>";
 
-OUTPUT.innerHTML += '<button onClick="start()">START</button>';
-
-OUTPUT.innerHTML = "Hello World"
+OUTPUT.innerHTML += '<button onclick="start()">START</button>';
 
 console.log("Hello " + name);
 console.log("You are " + age);
@@ -41,27 +42,33 @@ money = (money / 2) + 3;
 
 console.log("Then you get $3, now you have $" + money);
 
-
 /****************************
-Functions:
+Functions
 ****************************/
 function writeLine() {
-    OUTPUT.innerHTML += "<p>Less Code Matters</p>";
+  OUTPUT.innerHTML += "<p>Less Code Matters</p>";
 }
 
 function welcome() {
-    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+  OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
 }
 
 function displayProduct(_name, _price) {
-    OUTPUT.innerHTML += "<p>Price " + _name + ": $" + _price + " Each</p>";
+  OUTPUT.innerHTML += "<p>Price " + _name + ": $" + _price + " each</p>";
 }
 
 function start() {
-    OUTPUT.innerHTML += "<p>Start button clicked</p>";
+  OUTPUT.innerHTML += "<p>Start button clicked</p>";
 }
 
-// Function calls
+function lebron() {
+  let userName = NAME_FIELD.value;
+  OUTPUT.innerHTML += "<p>Hello " + userName + "!</p>";
+}
+
+/****************************
+Function calls
+****************************/
 welcome();
 writeLine();
 
--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,65 @@
-/* Paste your code from the last task */
+console.log("t11_data_types.js");
+console.log("Hello World");
+
+/****************************
+Variables
+****************************/
+let name = "Ragavan";
+let age = 15;
+let year = 2026;
+let money = 10;
+
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Main code
+****************************/
+OUTPUT.innerHTML = "<h3>Hello World</h3>";
+
+console.log("Hello " + name);
+console.log("You are " + age);
+console.log("The year is " + year);
+console.log("You have $" + money);
+
+/****************************
+Functions
+****************************/
+function writeLine() {
+  OUTPUT.innerHTML += "<p>Less Code Matters</p>";
+}
+
+function welcome() {
+  OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+
+function displayProduct(_name, _price) {
+  OUTPUT.innerHTML += "<p>Price " + _name + ": $" + _price + " each</p>";
+}
+
+function start() {
+  OUTPUT.innerHTML += "<p>Start button clicked</p>";
+}
+
+function lebron() {
+  let userName = NAME_FIELD.value;
+  let userAge = AGE_FIELD.value;
+  let userMoney = MONEY_FIELD.value;
+
+  OUTPUT.innerHTML =
+    "<h3>Hello " + userName + "!</h3>" +
+    "<p>You are " + userAge + " years old.</p>" +
+    "<p>You have $" + userMoney + " pocket money.</p>";
+}
+
+/****************************
+Function calls
+****************************/
+welcome();
+writeLine();
+
+displayProduct("Mc Medium", 4);
+displayProduct("Mc Large", 3);
+displayProduct("Mc XL", 2.50);
