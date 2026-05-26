1f27bc2770c9002d6076e1406719439a7077679c
Ragavan-knowsdtech <24058rr@hvhs.school.nz>
2026-05-27 11:24:28 +1200

    no

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,73 +1,40 @@
-console.log("t12_conditional_statements.js");
+console.log("t13_functions_return.js");
 console.log("Hello World");
 
 /****************************
 Variables
 ****************************/
-let name = "Ragavan";
-let age = 15;
-let year = 2026;
-let money = 10;
-
 const NAME_FIELD = document.getElementById("nameField");
 const AGE_FIELD = document.getElementById("ageField");
 const MONEY_FIELD = document.getElementById("moneyField");
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
 /****************************
-Main code
-****************************/
-OUTPUT.innerHTML = "<h3>Hello World</h3>";
-
-console.log("Hello " + name);
-console.log("You are " + age);
-console.log("The year is " + year);
-console.log("You have $" + money);
-
-/****************************
 Functions
 ****************************/
-function writeLine() {
-  OUTPUT.innerHTML += "<p>Less Code Matters</p>";
-}
-
-function welcome() {
-  OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
-}
-
-function displayProduct(_name, _price) {
-  OUTPUT.innerHTML += "<p>Price " + _name + ": $" + _price + " each</p>";
-}
-
-function start() {
-  OUTPUT.innerHTML += "<p>Start button clicked</p>";
+function calculateChange(_money, _price) {
+  return _money - _price;
 }
 
 function lebron() {
   let userName = NAME_FIELD.value;
   let userAge = AGE_FIELD.value;
   let userMoney = Number(MONEY_FIELD.value);
+  let chocolatePrice = 4;
 
-  OUTPUT.innerHTML =
-    "<h3>Hello " + userName + "!</h3>" +
-    "<p>You are " + userAge + " years old.</p>" +
-    "<p>You have $" + userMoney + " pocket money.</p>";
+  OUTPUT.innerHTML = "";
 
-  OUTPUT.innerHTML += "<p>A chocolate bar costs $4</p>";
+  OUTPUT.innerHTML += "<h3>Hello " + userName + "!</h3>";
+  OUTPUT.innerHTML += "<p>You are " + userAge + " years old.</p>";
+  OUTPUT.innerHTML += "<p>You have $" + userMoney + ".</p>";
+  OUTPUT.innerHTML += "<p>A chocolate bar costs $" + chocolatePrice + ".</p>";
 
-  if (userMoney >= 4) {
-    OUTPUT.innerHTML += "<p>You CAN afford a chocolate bar</p>";
+  if (userMoney >= chocolatePrice) {
+    let change = calculateChange(userMoney, chocolatePrice);
+
+    OUTPUT.innerHTML += "<p>You CAN afford a chocolate bar.</p>";
+    OUTPUT.innerHTML += "<p>You will get $" + change + " change.</p>";
   } else {
-    OUTPUT.innerHTML += "<p>Sorry you CAN'T afford a chocolate bar</p>";
+    OUTPUT.innerHTML += "<p>Sorry, you CAN'T afford a chocolate bar.</p>";
   }
 }
-
-/****************************
-Function calls
-****************************/
-welcome();
-writeLine();
-
-displayProduct("Mc Medium", 4);
-displayProduct("Mc Large", 3);
-displayProduct("Mc XL", 2.50);
--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,27 @@
-/* Paste your code from the last task */
+console.log("t15_for_loop.js");
+console.log("For Loops");
+
+/****************************
+Variables
+****************************/
+const VERSE_FIELD = document.getElementById("verseField");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Functions
+****************************/
+function singSong() {
+  let verses = Number(VERSE_FIELD.value);
+
+  OUTPUT.innerHTML = "";
+
+  for (let i = verses; i >= 1; i--) {
+
+    if (i === 1) {
+      OUTPUT.innerHTML += "<p>1 bottle of milk on the wall, 1 bottle of milk. Take one down, pass it around, no more bottles of milk on the wall.</p>";
+    } else {
+      OUTPUT.innerHTML += "<p>" + i + " bottles of milk on the wall, " + i + " bottles of milk. Take one down, pass it around, " + (i - 1) + " bottles of milk on the wall.</p>";
+    }
+
+  }
+}
