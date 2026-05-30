cc5aae40a6a28232aa55ab373e177682e31d39cc
Ragavan-knowsdtech <24058rr@hvhs.school.nz>
2026-05-30 16:06:02 +1200

    .

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,5 +1,5 @@
-console.log("t16_arrays_continued.js");
-console.log("Task 16 - Arrays 2: Shopping List");
+// Header comments
+console.log("t16_arrays_continued.js is loaded and running!");
 
 /****************************
 Variables
@@ -19,27 +19,31 @@ Functions
 function addItem() {
   let newItem = ITEM_FIELD.value;
 
- 
+  // Check if the input is not empty
   if (newItem !== "") {
-   
+    // 1. Add the item to the array
     shoppingList.push(newItem);
 
-   
-    OUTPUT.innerHTML = "You have added " + newItem + " to the list";
+    // 2. Display the confirmation message
+    OUTPUT.innerHTML = "<p>You have added <strong>" + newItem + "</strong> to the list.</p>";
 
-   
+    // 3. Clear the input box so it's ready for the next item
     ITEM_FIELD.value = ""; 
+  } else {
+    // If they click add without typing anything
+    OUTPUT.innerHTML = "<p>Please type an item first!</p>";
   }
 }
 
 function displayList() {
-
+  // Clear the output area
   OUTPUT.innerHTML = "";
 
+  // Print the heading
+  OUTPUT.innerHTML += "<p>These are the items on your shopping list:</p>";
 
-  OUTPUT.innerHTML += "These are the items on your shopping list:<br>";
-
+  // Loop through the array and print each item
   for (let i = 0; i < shoppingList.length; i++) {
-    OUTPUT.innerHTML += "- " + shoppingList[i] + "<br>";
+    OUTPUT.innerHTML += "<p>- " + shoppingList[i] + "</p>";
   }
 }
