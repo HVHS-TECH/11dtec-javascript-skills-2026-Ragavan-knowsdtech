f0692056f120b204ef6f0b7c297b713b15d9fd6a
Ragavan-knowsdtech <24058rr@hvhs.school.nz>
2026-06-18 15:14:00 +1200

    qwqwer

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,69 +1,48 @@
-// Header comments
+
 console.log("t16_arrays_continued.js is loaded and running!");
 
-let glassCount = 0;
-let standardCount = 0;
-let chocolateCount = 0;
+let shoppingList = [];
 
-const GLASS_PRICE = 3.99;
-const STANDARD_PRICE = 5.99;
-const CHOCOLATE_PRICE = 6.99;
-
-let userNameField = document.getElementById("nameField");
-
-
+//functions
 
 function addItem() {
 
-    let milkType = document.getElementById("milkType").value;
-    let quantity = Number(document.getElementById("quantity").value);
+    let item = document.getElementById("itemInput").value;
 
-    if (milkType === "glass") {
-        glassCount += quantity;
-    }
-    else if (milkType === "standard") {
-        standardCount += quantity;
-    }
-    else {
-        chocolateCount += quantity;
-    }
 
-    document.getElementById("orderList").innerHTML =
-        "Glass Bottle Milk: " + glassCount + "<br>" +
-        "Standard Milk: " + standardCount + "<br>" +
-        "Chocolate Milk: " + chocolateCount;
+   
+    shoppingList.push(item);
+
+
+  
+    document.getElementById("confirmation").innerHTML =
+    "You have added " + item + " to the list";
+
+
+  
+    document.getElementById("itemInput").value = "";
+
 }
 
-function checkout() {
 
-    let money = Number(document.getElementById("money").value);
 
-    let total =
-        (glassCount * GLASS_PRICE) +
-        (standardCount * STANDARD_PRICE) +
-        (chocolateCount * CHOCOLATE_PRICE);
+function displayList() {
 
-    if (money >= total) {
+    document.getElementById("confirmation").innerHTML =
+    "These are the items on your shopping list:";
 
-        let change = money - total;
 
-        document.getElementById("output").innerHTML =
-            "Order Successful!<br><br>" +
-            "Glass Bottle Milk: " + glassCount + "<br>" +
-            "Standard Milk: " + standardCount + "<br>" +
-            "Chocolate Milk: " + chocolateCount + "<br><br>" +
-            "Total Cost: $" + total.toFixed(2) + "<br>" +
-            "Money Given: $" + money.toFixed(2) + "<br>" +
-            "Change: $" + change.toFixed(2);
+    let list = document.getElementById("list");
+
+
+  
+    list.innerHTML = "";
+
+
+    for (let i = 0; i < shoppingList.length; i++) {
+
+        list.innerHTML += "<li>" + shoppingList[i] + "</li>";
 
     }
-    else {
 
-        let needed = total - money;
-
-        document.getElementById("output").innerHTML =
-            "Not enough money!<br>" +
-            "You need $" + needed.toFixed(2) + " more.";
-
-    }
 }
